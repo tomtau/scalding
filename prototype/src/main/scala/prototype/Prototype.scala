ef798ea1f58f2935a59a2e174103e5dd6f73d1de
Tomas Tauber <s0943263@sms.ed.ac.uk>
2013-06-07 15:50:49 +0200

    stylistic changes + new test

--- prototype/src/main/scala/prototype/Prototype.scala
+++ prototype/src/main/scala/prototype/Prototype.scala
@@ -1,28 +1,28 @@
 package prototype
 
-object Main {
+object Prototype {
 
-  abstract class MatrixChain
+  sealed trait MatrixChain
   case class MultOp(left: MatrixChain, right: MatrixChain) extends MatrixChain
-  case class Matrix(id: Int) extends MatrixChain  
-  
+  case class Matrix(id: Int) extends MatrixChain
+
   /**
    * p - array with dimensions of the chain of matrices
-   * p_{i − 1} × p_{i} is multiplied by p_{i} × p_{i + 1} 
+   * p_{i − 1} × p_{i} is multiplied by p_{i} × p_{i + 1}
    */
-  def MemMatrix(p: Array[Int]): (Int, MatrixChain) = {
+  def memMatrix(p: Array[Int]): (Int, MatrixChain) = {
     // costs array for each subchain
     val m = Array.fill(p.length, p.length)(-1)
     // split marker array
     val s = Array.fill(p.length, p.length)(-1)
     
-    def MemMatrixChain(p: Array[Int], i: Int, j: Int): Int = {
+    def memMatrixChain(p: Array[Int], i: Int, j: Int): Int = {
       if (m(i)(j) != -1) m(i)(j)
       if (i == j) m(i)(j) = 0
       else {
         m(i)(j) = Int.MaxValue
         for (k <- i to (j - 1)) {
-          val cost = MemMatrixChain(p, i, k) + MemMatrixChain(p, k + 1, j) + p(i-1)*p(k)*p(j)
+          val cost = memMatrixChain(p, i, k) + memMatrixChain(p, k + 1, j) + p(i-1)*p(k)*p(j)
           if (cost < m(i)(j)) { 
             m(i)(j) = cost
             s(i)(j) = k
@@ -33,20 +33,20 @@ object Main {
       m(i)(j)
     }
     
-    def MultOrder(i: Int, j: Int): MatrixChain = {
+    def multOrder(i: Int, j: Int): MatrixChain = {
       if (i == j) Matrix(i)
       else {
         val k = s(i)(j)
-        val X = MultOrder(i, k)
-        val Y = MultOrder(k+1, j)
+        val X = multOrder(i, k)
+        val Y = multOrder(k+1, j)
         MultOp(X, Y)
       }
       
     }
     
-    val x = MemMatrixChain(p, 1, p.length - 1)
+    val x = memMatrixChain(p, 1, p.length - 1)
 
-    (x, MultOrder(1, p.length - 1))
+    (x, multOrder(1, p.length - 1))
   }
 
 }
--- prototype/src/test/scala/prototype/TestPrototype.scala
+++ prototype/src/test/scala/prototype/TestPrototype.scala
@@ -5,13 +5,20 @@ import org.scalatest.FunSuite
 
 class TestMain extends FunSuite {
   
+  test("only two matrices") {
+    val p = Array(30, 35, 15)
+    val result = Prototype.memMatrix(p)
+    expect(15750) {result._1}
+    expect(Prototype.MultOp(Prototype.Matrix(1), Prototype.Matrix(2))) {result._2}
+  }  
+  
   test("basic test of dynamic programming optimization of a matrix multiplication chain") {
     val p = Array(30, 35, 15, 5, 10, 20, 25)
-    val result = Main.MemMatrix(p)
+    val result = Prototype.memMatrix(p)
     expect(15125) {result._1}
     // ((A1(A2 A3))((A4 A5) A6)
-    expect(Main.MultOp(Main.MultOp(Main.Matrix(1), Main.MultOp(Main.Matrix(2),Main.Matrix(3))),
-        Main.MultOp(Main.MultOp(Main.Matrix(4),Main.Matrix(5)),Main.Matrix(6))
+    expect(Prototype.MultOp(Prototype.MultOp(Prototype.Matrix(1), Prototype.MultOp(Prototype.Matrix(2),Prototype.Matrix(3))),
+        Prototype.MultOp(Prototype.MultOp(Prototype.Matrix(4),Prototype.Matrix(5)),Prototype.Matrix(6))
         )
         ) {result._2}
   }
