b2d5179031566b892dcba24041ca60095ae8c10e
Tomas Tauber <s0943263@sms.ed.ac.uk>
2013-06-10 21:46:25 +0200

    refactor of cost and split markers names

--- prototype/src/main/scala/prototype/Prototype.scala
+++ prototype/src/main/scala/prototype/Prototype.scala
@@ -9,10 +9,10 @@ object Prototype {
   case class Literal(dimensions: (Int, Int), sparsity: Float) extends MatrixFormula
 
   def optimizeProductChain(p: IndexedSeq[Literal]): (Float, MatrixFormula) = {
-    // costs for each subchain
-    val m = HashMap.empty[(Int,Int), Float]
-    // split markers
-    val s = HashMap.empty[(Int,Int), Int]
+
+    val subchainCosts = HashMap.empty[(Int,Int), Float]
+
+    val splitMarkers = HashMap.empty[(Int,Int), Int]
 
     def max(one: Float, two: Float, three: Float): Float = {
       if (one > two) {
@@ -23,28 +23,28 @@ object Prototype {
     }
 
     def computeCosts(p: IndexedSeq[Literal], i: Int, j: Int): Float = {
-      if (m.contains((i,j))) m((i,j))
-      if (i == j) m.put((i,j), 0)
+      if (subchainCosts.contains((i,j))) subchainCosts((i,j))
+      if (i == j) subchainCosts.put((i,j), 0)
       else {
-        m.put((i,j), Int.MaxValue)
+        subchainCosts.put((i,j), Int.MaxValue)
         for (k <- i to (j - 1)) {
           val cost = computeCosts(p, i, k) + computeCosts(p, k + 1, j) +
                      p(i).dimensions._1 * p(k).dimensions._2 * p(j).dimensions._2 *
                      max(p(i).sparsity, p(k).sparsity, p(j).sparsity)
-          if (cost < m((i,j))) {
-            m.put((i,j), cost)
-            s.put((i,j), k)
+          if (cost < subchainCosts((i,j))) {
+            subchainCosts.put((i,j), cost)
+            splitMarkers.put((i,j), k)
           }
         }
       }
 
-      m((i,j))
+      subchainCosts((i,j))
     }
 
     def generatePlan(i: Int, j: Int): MatrixFormula = {
       if (i == j) p(i)
       else {
-        val k = s((i,j))
+        val k = splitMarkers((i,j))
         val X = generatePlan(i, k)
         val Y = generatePlan(k + 1, j)
         Product(X, Y)
