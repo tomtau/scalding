adca7c85536faa1d5068b24ec413871a311baa9f
Tomas Tauber <s0943263@sms.ed.ac.uk>
2013-06-15 20:54:52 +0200

    Style changes

--- prototype/src/main/scala/prototype/Mockup.scala
+++ prototype/src/main/scala/prototype/Mockup.scala
@@ -2,46 +2,46 @@ package prototype
 
 import com.twitter.scalding.mathematics.SizeHint
 import com.twitter.scalding.mathematics.NoClue
-import com.twitter.algebird.{Monoid, Ring}
+import com.twitter.algebird.{ Monoid, Ring }
 
 object Mockup {
 
-  sealed trait Matrix extends Iterable[Iterable[Double]]  {
+  sealed trait Matrix extends Iterable[Iterable[Double]] {
     def +(that: Matrix): Matrix = Sum(this, that)
     def *(that: Matrix): Matrix = Product(this, that)
     val sizeHint: SizeHint = NoClue
   }
 
-  case class Product(left: Matrix, right: Matrix)(implicit ring : Ring[Double]) extends Matrix {
+  case class Product(left: Matrix, right: Matrix)(implicit ring: Ring[Double]) extends Matrix {
     override val sizeHint = left.sizeHint * right.sizeHint
 
     def iterator: Iterator[Iterable[Double]] = toList().iterator
-    
+
     override def toList(): List[Iterable[Double]] = {
       (for {
         keyL <- left.view
         keyR <- right.view.transpose
-      } yield (keyL,keyR)).zipWithIndex.groupBy(row => row._2 / left.size).toSeq.sortBy(a => a._1). // get joined row-cols in order
-      map(x => x._2.map(y => y._1).map(z => (z._1 zip z._2). // strip off indices and zip entry elements
-      map(els => ring.times(els._1,els._2)). // pairwise multiplication
-      reduce((el1,el2) => ring.plus(el1,el2)))) // sum products to the resulting element
-      .toList
+      } yield (keyL, keyR)).zipWithIndex.groupBy(row => row._2 / left.size).toSeq.sortBy(a => a._1). // get joined row-cols in order
+        map(x => x._2 map (y => y._1) map (z => (z._1 zip z._2) // strip off indices and zip entry elements
+        map (els => ring.times(els._1, els._2)) // pairwise multiplication
+        reduce ((el1, el2) => ring.plus(el1, el2)))) // sum products to the resulting element
+        toList
     }
   }
-  
-  case class Sum(left: Matrix, right: Matrix)(implicit mon : Monoid[Double]) extends Matrix {
+
+  case class Sum(left: Matrix, right: Matrix)(implicit mon: Monoid[Double]) extends Matrix {
     override val sizeHint = left.sizeHint + right.sizeHint
 
     def iterator: Iterator[Iterable[Double]] = toList().iterator
-    
+
     override def toList(): List[Iterable[Double]] = {
-      (left.view zip right.view) map (cols => (cols._1 zip cols._2) map (x => mon.plus(x._1,x._2))) toList
+      (left.view zip right.view) map (cols => (cols._1 zip cols._2) map (x => mon.plus(x._1, x._2))) toList
     }
   }
-  
+
   case class Literal(override val sizeHint: SizeHint, val vals: List[List[Double]]) extends Matrix {
     def iterator: Iterator[Iterable[Double]] = vals.iterator
-    
+
     override def toList(): List[List[Double]] = vals
   }
 }
